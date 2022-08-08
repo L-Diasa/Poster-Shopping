44d7511c840511e51c98f55b2134b1b272e57721
L-Diasa <likadiasa@gmail.com>
2022-08-08 23:59:01 +0400

    Commit

---
+++ src/hooks/useHover.js
@@ -0,0 +1,30 @@
+import {useState, useEffect, useRef} from "react"
+
+function useHover() {
+    const [hovered, setHovered] = useState(false)
+    const ref = useRef(null)
+    
+    function enter() {
+        setHovered(true)
+    }
+    
+    function leave() {
+        setHovered(false)
+    }
+    
+    useEffect(() => {
+
+        ref.current.addEventListener("mouseenter", enter)
+        ref.current.addEventListener("mouseleave", leave)
+        return () => { 
+            if(ref.current){
+                ref.current.onmouseenter = null
+                ref.current.onmouseleave = null
+            }
+        }
+    }, [])
+    
+    return [hovered, ref]
+}
+
+export default useHover
